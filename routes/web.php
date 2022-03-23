82657404684f5fc7ddc2b19a8111b0e0e62c6fda
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2022-03-23 17:11:17 +0000

    saving

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本の一覧表示(books.blade.php)
+*/
+Route::get('/', function () {
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books',[
+        'books'=>$books
+    ]);
+});
+
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+
+    //バリデーション
+    $validator = Validator($request->all(),[
+        'item_name' => 'required|max:255',
+    ]);
+    
+    //バリデーション：エラー
+    if($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+    //Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
+    
+});
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
